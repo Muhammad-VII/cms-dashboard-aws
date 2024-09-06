71bff3e13ee4c9368d4d531d50db6e0ac9eddcae
Mohammed Akmal <83099824+Muhammad-VII@users.noreply.github.com>
2024-09-06 18:12:16 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+#!/bin/bash
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+npm run start:prod:pm2
